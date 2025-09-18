cc25470ea8bfbeab8426bf525c5eec2c5022fbc4
Simon Oser <simon.oser@gmail.com>
2025-09-18 17:03:50 +0200

    Renames package to dev.smo

--- src/main/java/dev/smo/rabbitmq/SpringBootRabbitmqApplication.java
+++ src/main/java/dev/smo/rabbitmq/SpringBootRabbitmqApplication.java
@@ -1,4 +1,4 @@
-package io.eyce.rabbitmq;
+package dev.smo.rabbitmq;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/test/java/dev/smo/rabbitmq/SpringBootRabbitmqApplicationTests.java
+++ src/test/java/dev/smo/rabbitmq/SpringBootRabbitmqApplicationTests.java
@@ -1,4 +1,4 @@
-package io.eyce.rabbitmq;
+package dev.smo.rabbitmq;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
